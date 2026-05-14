12b2aef50684c7885a41adaeb7f27d4127f0520d
junayd-elahi <je4857p@gre.ac.uk>
2026-05-14 11:13:21 +0100

    Player left right movement

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
 	dt = 0
 	x = SCREEN_WIDTH / 2
 	y = SCREEN_HEIGHT / 2
-	
+
 	player = Player(x, y)
 	while True:
 		log_state()
@@ -23,10 +23,13 @@ def main():
 			if event.type == pygame.QUIT:
 				return
 
+		dt = clock.tick(60)/1000
+
+		player.update(dt)
+
 		screen.fill("black")
 		player.draw(screen)
 		pygame.display.flip()
-		dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
 import pygame
 
 
@@ -16,7 +16,20 @@ class Player(CircleShape):
     		b = self.position - forward * self.radius - right
     		c = self.position - forward * self.radius + right
     		return [a, b, c]
-	
+
 
 	def draw(self,screen):
 		pygame.draw.polygon(screen,"white", self.triangle(), LINE_WIDTH)
+
+
+	def rotate(self,dt):
+		self.rotation += PLAYER_TURN_SPEED * dt
+
+
+	def update(self, dt):
+        	keys = pygame.key.get_pressed()
+
+        	if keys[pygame.K_a]:
+            		self.rotate(-dt)
+        	if keys[pygame.K_d]:
+            		self.rotate(dt)
