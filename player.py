2464f54a4bc0f8877f90f4297b71967f8b7e9680
junayd-elahi <je4857p@gre.ac.uk>
2026-05-14 11:31:04 +0100

    full player movement

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
 import pygame
 
 
@@ -10,12 +10,12 @@ class Player(CircleShape):
 
 	# in the Player class
 	def triangle(self):
-    		forward = pygame.Vector2(0, 1).rotate(self.rotation)
-    		right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-    		a = self.position + forward * self.radius
-    		b = self.position - forward * self.radius - right
-    		c = self.position - forward * self.radius + right
-    		return [a, b, c]
+			forward = pygame.Vector2(0, 1).rotate(self.rotation)
+			right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+			a = self.position + forward * self.radius
+			b = self.position - forward * self.radius - right
+			c = self.position - forward * self.radius + right
+			return [a, b, c]
 
 
 	def draw(self,screen):
@@ -27,9 +27,19 @@ class Player(CircleShape):
 
 
 	def update(self, dt):
-        	keys = pygame.key.get_pressed()
+			keys = pygame.key.get_pressed()
 
-        	if keys[pygame.K_a]:
-            		self.rotate(-dt)
-        	if keys[pygame.K_d]:
-            		self.rotate(dt)
+			if keys[pygame.K_a]:
+					self.rotate(-dt)
+			if keys[pygame.K_d]:
+					self.rotate(dt)
+			if keys[pygame.K_w]:
+				self.move(dt)
+			if keys[pygame.K_s]:
+				self.move(-dt)
+
+	def move(self,dt):
+		unit_vector = pygame.Vector2(0,1)
+		rotated_vector = unit_vector.rotate(self.rotation)
+		rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+		self.position += rotated_with_speed_vector
